aca272f79b40a81aa280dd85d12cd82d8f2ac7c8
Jon Eastin <picardncce@gmail.com>
2022-07-05 18:33:31 -0700

    more work, still not having comments show yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+import TodosList from './TodosList'
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+	const [users, setUsers] = useState([])
+	const [currentUser, setCurrentUser] = useState({})
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((data) => setUsers(data))
+	}, [])
+
+	const userList = users.map((user, index) => {
+		return (
+			<li className='list-group-item' key={index}>
+				<h6>{user.name}</h6>
+				<button
+					onClick={() => {
+						setCurrentUser(user)
+					}}
+					type='button'
+					name={`${user.name}`}>
+					Show Todos
+				</button>
+			</li>
+		)
+	})
+
+	return (
+		<div className='App container'>
+			<h1>Users</h1>
+			<div className='row'>
+				<div className='col-6'>
+					<ul className='list-group'>{userList}</ul>
+				</div>
+				<div className='col-6'>
+					<TodosList users={currentUser} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/TodosList.js
+++ src/TodosList.js
@@ -1,7 +1,50 @@
-export default function TodosList() {
-  return (
-    <section>
-      Todos List
-    </section>
-  );
+import { useState, useEffect, Switch } from 'react'
+
+function TodosList({ users: { id, name } }) {
+	const [todos, setTodos] = useState([])
+	const [toggle, setToggle] = useState(false)
+	let url = 'https://jsonplaceholder.typicode.com/users/' + id + '/todos'
+
+	const triggerToggle = () => {
+		setToggle(!toggle)
+	}
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => setTodos(data))
+	}, [url])
+
+	const incompleteTodos = todos
+		.filter((todo) => todo.completed === false)
+		.map((todo, index) => {
+			return (
+				<li className='list-group-item' key={index}>
+					<p>{todo.title}</p>
+				</li>
+			)
+		})
+
+	const todosList = todos.map((todo, index) => {
+		return (
+			<li className='list-group-item' key={index}>
+				<p>{todo.title}</p>
+			</li>
+		)
+	})
+
+  					<div value={true}>
+						<h3>{name}</h3>
+						<p>Showing incomplete Todos</p>
+						<button onClick={triggerToggle}>Toggle filter</button>
+						<ul className='container list-group'>{todosList}</ul>
+					</div>
+
+	return (
+		<section>
+      
+		</section>
+	)
 }
+
+export default TodosList
